41de216b196c1233a7a39461058253fe56df71cc
bakulevalena <bakuleva.lena@gmail.com>
2016-10-17 23:05:29 +0300

    Commit our project

---
+++ src/GameState/GameState.java
@@ -0,0 +1,16 @@
+package GameState;
+
+/**
+ * Created by Шпакулева on 17.10.2016.
+ */
+public abstract class GameState {
+
+    protected GameStateManager gsm;
+
+    public abstract void init();
+    public abstract void update();
+    public abstract void draw(java.awt.Graphics2D g);
+    public abstract void keyPressed(int k);
+    public abstract void keyReleased(int k);
+
+}
---
+++ src/Main/GamePanel.java
@@ -0,0 +1,115 @@
+package Main;
+
+import GameState.GameStateManager;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+import java.awt.event.*;
+/**
+ * Created by Шпакулева on 12.10.2016.
+ */
+public class GamePanel extends JPanel implements Runnable, KeyListener{
+
+    public static final int WIDTH = 320;
+    public static final int HEIGHT = 240;
+    public static final int SCALE = 2;
+
+    private Thread thread;
+    private  boolean running;
+    private int FPS = 60;
+    private long targetTime = 1000/FPS;
+
+    private BufferedImage image;
+    private Graphics2D g;
+
+    private GameStateManager gsm;
+
+    public GamePanel() {
+
+        super();
+        setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+        setFocusable(true);
+        requestFocus();
+    }
+
+    public void addNotify() {
+        super.addNotify();
+        if (thread == null) {
+            thread = new Thread(this);
+            addKeyListener(this);
+            thread.start();
+        }
+    }
+    public void init() {
+
+        image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_3BYTE_BGR);
+        g = (Graphics2D) image.getGraphics();
+
+        running = true;
+
+        gsm = new GameStateManager();
+    }
+
+    public void run(){
+        init();
+
+        long start;
+        long elapsed;
+        long wait;
+
+        while(running){
+
+            start = System.nanoTime();
+
+            update();
+            draw();
+            drawToScreen();
+
+            elapsed = System.nanoTime() - start;
+
+            wait = targetTime - elapsed /  1000000;
+            if (wait<0){
+                wait = 5;
+            }
+            try{
+                Thread.sleep(wait);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+    private void update(){
+        gsm.update();
+    }
+    private void draw(){
+        gsm.draw(g);
+    }
+    private void drawToScreen(){
+
+        Graphics g2 = getGraphics();
+        g2.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+        g2.dispose();
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent key) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent key) {
+        gsm.keyPressed(key.getKeyCode());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent key) {
+        gsm.keyReleased(key.getKeyCode());
+    }
+}
+
+
+
